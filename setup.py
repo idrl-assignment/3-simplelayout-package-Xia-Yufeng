7c210835747e29ec1df39048417cc14ba6754a8b
xia-yufeng <714811246@qq.com>
2020-11-21 19:00:36 +0800

    202011211900

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.3.1',  # Required
+    version='0.3.2',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -110,7 +110,7 @@ setup(
 
     # When your source code is in a subdirectory under the project root, e.g.
     # `src/`, it is necessary to specify the `package_dir` argument.
-    package_dir={'': 'src/simplelayout'},  # Optional
+    package_dir={'': 'src'},  # Optional
 
     # You can just specify package directories manually here if your project is
     # simple. Or you can use find_packages().
@@ -121,7 +121,7 @@ setup(
     #
     #   py_modules=["my_module"],
     #
-    packages=find_packages(),  # Required
+    packages=find_packages('src'),  # Required
 
     # Specify which Python versions you support. In contrast to the
     # 'Programming Language' classifiers above, 'pip install' will check this
---
+++ src/simplelayout/__init__.py
@@ -0,0 +1,2 @@
+from .__main__ import *
+from __future__ import absolute_import
